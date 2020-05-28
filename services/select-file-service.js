b6959219110bb6834d9a5f592ee3eeb9061a8d1f
Eswaran Arumugam <eswaran.arumugam@outlook.com>
2020-05-28 11:51:50 +0100

    Completed module

--- services/select-file-service.js
+++ services/select-file-service.js
@@ -1,6 +1,9 @@
 const fs = require("fs");
 const path = require("path");
-const dir = process.cwd();
+let dir;
+exports.setcwd = function (cwd) {
+    dir = cwd;
+} 
 
 function getDirectoryContents(files, currentDir, query) {
     const data = [];
